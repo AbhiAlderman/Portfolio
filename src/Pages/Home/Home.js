37dc59946f08025a27bb99c9106891b3c4fe0d86
Abhi Alderman <abhialderman@gmail.com>
2022-06-10 19:25:31 -0700

    added folders for upcoming components

---
+++ src/Pages/Home/Home.js
@@ -0,0 +1,13 @@
+import React from 'react';
+import './Home.css';
+
+
+export default function Home() {
+    return (
+        <div className="page" id="home">
+            <div className="page-text" id="home-text">
+                <div className="text"> THIS IS JUST A TEST</div>
+            </div>
+        </div>
+    )
+}
--- src/index.js
+++ src/index.js
@@ -2,11 +2,12 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import './index.css';
 import reportWebVitals from './reportWebVitals';
+import Home from './Pages/Home/Home';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <></>
+    <Home />
   </React.StrictMode>
 );
 
