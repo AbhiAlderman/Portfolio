e16b4bffd763ee501e52e090cd57a16375923ee7
Abhi Alderman <abhialderman@gmail.com>
2022-06-15 23:47:08 -0700

    starting to update projects page

--- src/Pages/Projects/projects.js
+++ src/Pages/Projects/projects.js
@@ -9,13 +9,10 @@ import jeffrey from './Visuals/jeffrey.jpg';
 import flip from './Visuals/manual_flip.gif';
 import cutter from './Visuals/cutter.png';
 import crusader from './Visuals/bayarea_crusader.jpg';
-const Projects = () => {
-  return (
-    <div>
-      <h1>This will be for projects</h1>
-      <p> Temporarily storing some visuals here</p>
-      <main class="grid-layout">
-        <img src={robot1} className="visuals" alt="Benchmark Robot 1"/>
+import cruuz from './Visuals/cruuz.png';
+import cruuz_website from './Visuals/cruuz_website.JPG';
+/*
+<img src={robot1} className="visuals" alt="Benchmark Robot 1"/>
         <img src={robot2} className="visuals" alt="Benchmark Robot 2"/>
         <img src={flip} className="visuals" alt="Me manually flipping part"/>
         <img src={robot3} className="visuals" alt="Benchmark Robot 3"/>
@@ -24,7 +21,31 @@ const Projects = () => {
         <img src={jeffrey} className="visuals" alt="jeffrey"/>
         <img src={cutter} className="visuals" alt="Ribbon cutting robot"/>
         <img src={crusader} className="visuals" alt="Crusader at Bay Area Science Festival"/>
-      </main>
+      
+*/
+const Projects = () => {
+  return (
+    <div className="projects">
+      <h1 className="title">PROJECTS</h1>
+      <p> Display of my industry work and personal projects</p>
+      <br></br>
+      <br></br>
+      <h2 className="type"> Industry Experience</h2>
+      <br></br>
+      <h2 className="label">Benchmark Electronics Robot</h2>
+      <div className="benchmark">
+        <img src={robot2} className="visuals" alt="Benchmark Robot 2"/>
+        <p className="description"> At Benchmark Electronics I programmed and operated a KUKA Robotic Arm. 
+        I programmed the Robot arm to autonomously pick up tools, pick up and flip products, deburr holes, polish surfaces,
+        and brush edges. <br></br><br></br> Further visuals and description available upon request </p>
+        <img src={robot3} className="visuals" alt="Benchmark Robot 3"/>
+      </div>
+      <br></br>
+      <div className="cruuz">
+        <img src={cruuz} alt="Cruuz inc Logo"/>
+        <p className="description"> I worked at cruuz blah blah</p>
+        <img src={cruuz_website} className="cruuzWeb" alt="Cruuz inc website screencap"/>
+      </div>
     </div>
   );
 };
