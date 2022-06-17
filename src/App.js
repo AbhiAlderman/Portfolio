1d7c3ce5f3694858f0246da6b15fa76b11ebeacf
Abhi Alderman <abhialderman@gmail.com>
2022-06-16 22:03:48 -0700

    fixed build

--- src/App.js
+++ src/App.js
@@ -1,5 +1,4 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
+import React, {useEffect} from 'react';
 import './App.css';
 import 'bootstrap/dist/css/bootstrap.min.css';
 import { BrowserRouter as Router, Routes, Route}
