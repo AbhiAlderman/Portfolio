1bd1127ff3fe86cc79efb1fc12056ce020dca6a6
Abhi Alderman <abhialderman@gmail.com>
2022-06-16 19:43:59 -0700

    fixed multi-line string error

--- src/Pages/Projects/projects.js
+++ src/Pages/Projects/projects.js
@@ -66,12 +66,12 @@ const industryProjects = [
     leftVisualSubtitle: 'Cruuz Logo',
     rightVisual: cruuz_website,
     rightVisualSubtitle: 'In-App Demo',
-    description:'Cruuz Inc. is a startup focused on providing \
-              haircut and beauty appointment booking through an all-encompassing app. Users are able \
-              to use the app to book an appointment from multiple salons in the area, much like \
-              DoorDash or UberEats. I am currently working as a Software Engineering Intern\
-              here. My task is focused on updating the main website , and developing a pintrest-like \
-              gallery for the app.',
+    description:'Cruuz Inc. is a startup focused on providing \n' + 
+              'haircut and beauty appointment booking through an all-encompassing app. Users are able \n' + 
+              'to use the app to book an appointment from multiple salons in the area, much like \n' + 
+              'DoorDash or UberEats. I am currently working as a Software Engineering Intern \n' + 
+              'here. My task is focused on updating the main website , and developing a pintrest-like \n' + 
+              'gallery for the app.',
     links: [['Company Website', 'https://www.cruuzinc.com/']]
   },
   {
@@ -82,12 +82,12 @@ const industryProjects = [
     leftVisualSubtitle: 'Polishing Surface',
     rightVisual: robot3,
     rightVisualSubtitle: 'Deburring Holes',
-    description: "At Benchmark Electronics I programmed and operated a KUKA Robotic Arm. \
-    I programmed the Robot arm to autonomously pick up tools, pick up and flip products, deburr holes, polish surfaces\
-    and brush edges. The potters wheel was equiped with an encoder and used to autonomously spin the different parts around. \
-    I used this to align holes to be deburred and to sping the product in the opposite direction as the arm, \
-    allowing for a smoother finish when polishing. This was all done in the KUKA programming language, where I utilized \
-    basic data structures and different loops to complete repetitive routines. Further visuals and description available upon request. ",
+    description: 'At Benchmark Electronics I programmed and operated a KUKA Robotic Arm. \n' + 
+                  'I programmed the Robot arm to autonomously pick up tools, pick up and flip products, deburr holes, polish surfaces \n' +
+                  'and brush edges. The potters wheel was equiped with an encoder and used to autonomously spin the different parts around. \n' + 
+                  'I used this to align holes to be deburred and to sping the product in the opposite direction as the arm, \n' +
+                  'allowing for a smoother finish when polishing. This was all done in the KUKA programming language, where I utilized \n' +
+                  'basic data structures and different loops to complete repetitive routines. Further visuals and description available upon request.',
     links: []
   }
 ];
@@ -101,10 +101,10 @@ const personalProjects = [
     leftVisualSubtitle: '',
     rightVisual: '',
     rightVisualSubtitle: '',
-    description: 'I created this website in Summer 2022 to showcase my skills in Software.\
-        This was my first relatively large front-end project. I learned everything I needed to know from youtube,\
-        stack overflow, documentation, and friends. It took approximately a week for me to finish the rough draft\
-        of this site, and I have been continously updating it as I see fit from there.',
+    description: 'I created this website in Summer 2022 to showcase my skills in Software. \n' +
+                  'This was my first relatively large front-end project. I learned everything I needed to know from youtube, \n' +
+                  'stack overflow, documentation, and friends. It took approximately a week for me to finish the rough draft \n' +
+                  'of this site, and I have been continously updating it as I see fit from there.',
     links: [['Github Repo', 'https://github.com/AbhiAlderman/Portfolio']]
   },
   {
@@ -115,39 +115,16 @@ const personalProjects = [
     leftVisualSubtitle: 'Webserver from Microcontroller',
     rightVisual: eyes,
     rightVisualSubtitle: 'Animatronic Eyes Tracking a Face',
-    description: 'Created a pair of Animatronic Eyes with two other students.\
-      Eyes were modeled using TinkerCAD, the PCB was fully designed by us in KiCAD and manufactured in China,\
-      and the servos were controlled by an ESP32-CAM microcontroller. \
-      I programmed the microcontroller in C using the Arduino program, and was able to succesfully get a Webserver running\
-      by using the microcontroller as an access point. The webserver allowed me to see what the camera saw and fine-tune the\
-      facial recognition and tracking. The camera was able to locate the position of a face, and I used this position to \
-      control the position of the servos accordingly. This made it seem like the eyes would "follow" a face as they moved around.',
+    description: 'Created a pair of Animatronic Eyes with two other students. \n' +
+                  'Eyes were modeled using TinkerCAD, the PCB was fully designed by us in KiCAD and manufactured in China, \n' +
+                  'and the servos were controlled by an ESP32-CAM microcontroller. \n' +
+                  'I programmed the microcontroller in C using the Arduino program, and was able to succesfully get a Webserver running \n' +
+                  'by using the microcontroller as an access point. The webserver allowed me to see what the camera saw and fine-tune the \n' +
+                  'facial recognition and tracking. The camera was able to locate the position of a face, and I used this position to \n' +
+                  'control the position of the servos accordingly. This made it seem like the eyes would "follow" a face as they moved around.',
     links: [['Github Repo', 'https://github.com/AbhiAlderman/Animatronic-Eyes']]
   }
 ];
 export default Projects;
 
-/* 
 
-*/
-
-/* 
-
-        <h2 className="label">Animatronic Eyes</h2>
-          <h3 className="position">Technologies:Arduino, C, C++, C#, TinkerCAD, KiCAD</h3>
-            <div className="project-grid">
-              <div>
-                <img src={eyesWebpage} className="visuals" alt="Animatronic Webpage"/>
-                <p className="description">Webserver from Microcontroller</p>
-              </div>
-              <p className="description">Created a pair of Animatronic Eyes with two other students.
-              Eyes were modeled using TinkerCAD, the PCB was fully designed by us in KiCAD and manufactured in China,
-              and the servos were controlled by an ESP32-CAM microcontroller.<br></br>
-              </p>
-              <div>
-                <img src={eyes} className="animatronicWebpage" alt="Animatronic Eyes"/>
-                <p className="description">Animatronic Eyes tracking face</p>
-              </div>
-              
-            </div>
-            */
